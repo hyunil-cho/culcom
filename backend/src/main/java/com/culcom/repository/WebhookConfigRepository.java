7ec07f33ff58c3deaf4d4860bf917e641ffc16db
hyunil <jackandjills@naver.com>
2026-04-06 15:31:00 +0900

    webhook 코드 삭제

--- backend/src/main/java/com/culcom/repository/WebhookConfigRepository.java
+++
@@ -1,9 +0,0 @@
-package com.culcom.repository;
-
-import com.culcom.entity.webhook.WebhookConfig;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface WebhookConfigRepository extends JpaRepository<WebhookConfig, Long> {
-    List<WebhookConfig> findByBranchSeqOrderByCreatedDateDesc(Long branchSeq);
-}
